7c482614e174c3c12eef13d0ea5e89986b85e171
DaveenDev <daveendev07@gmail.com>
2020-04-01 16:52:28 +0800

    Added fake answers 2

--- app/Answer.php
+++ app/Answer.php
@@ -13,4 +13,23 @@ class Answer extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+
+    public function getBodyHtmlAttribute(){
+        return  \Parsedown::instance()->text($this->body);
+     }
+
+    public static function boot(){
+        parent::boot();
+
+        //create an event handler when Answer is created
+
+        static::created(function($answer){
+            $answer->question->increment('answers_count');
+        });
+
+        //saved event handle
+        static::saved(function ($answer){
+            
+        });
+    }
 }
